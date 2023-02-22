67dd6339da76091f789611f356bc54def6997674
shubhamchandge <shubhamchandge@gmail.comm>
2023-02-22 11:22:25 +0530

    i like ui

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-   <div> shuba</div>
+   <div> 
+    <h1  data-ns-test = " project-name">project-name</h1>
+
+    <p data-ns-test = "project-description">project-description</p>
+   </div>
   )
 }
 
