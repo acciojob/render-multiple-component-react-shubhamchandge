2049705d84c86b03319d82ee1958fb41a1550505
shubhamchandge <shubhamchandge@gmail.comm>
2023-02-22 11:15:26 +0530

    i like coding

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,7 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main">
-      <h1 data-ns-test ="project-name"> project-Name</h1>
-      <br/><br/>
-      <h3 data-ns-test = "project-description" > Project-description</h3>
-    </div>
+   <div> shuba</div>
   )
 }
 
